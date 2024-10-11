85edc9d5f65aafd817804b076ab049fcbd002867
Alan Francis <alan.prime567@gmail.com>
2024-10-11 15:39:17 +0530

    initial page setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        nats: "var(--font-nats)",
+        ndot: "var(--font-ndot)",
+      },
     },
   },
   plugins: [],
